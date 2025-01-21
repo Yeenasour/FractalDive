99677234d500b80ab556a04438a84eb72b568dae
Yeenasour <wilmer.greppe@gmail.com>
2025-01-21 16:37:33 +0100

    Completed simple program, updated dependencies and updated LICENSE

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,130 @@
-#include "config.h"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <iostream>
+//#include <imgui/imgui.h>
+//#include <imgui/backends/imgui_impl_glfw.h>
+//#include <imgui/backends/imgui_impl_opengl3.h>
 
-int main() {
-	
+#include <fstream>
+#include <sstream>
+#include <string>
+
+static std::string readFile(const std::string& filePath) {
+    std::ifstream file(filePath);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file: " << filePath << std::endl;
+        return "";
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+
+    return buffer.str();
+}
+
+static GLuint CompileShader(GLuint type, const std::string& source)
+{
+	GLuint shader = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(shader, 1, &src, nullptr);
+	glCompileShader(shader);
+
+	GLint res;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &res);
+	if (res == GL_FALSE)
+	{
+		int len;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+		char* msg = (char*)alloca(len * sizeof(char));
+		glGetShaderInfoLog(shader, len, &len, msg);
+		std::cout << "Shader compilation failed! (" << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << ")" << std::endl;
+		std::cout << msg << std::endl;
+		glDeleteShader(shader);
+		return 0;
+	}
+
+	return shader;
+}
+
+static GLuint CreateShaderProgram(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	GLuint program = glCreateProgram();
+
+	GLuint vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	GLuint fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
+int main()
+{
 	GLFWwindow* window;
 
 	if (!glfwInit()) {
-		std::cout << "GLFW couldn't start" << std::endl;
+		std::cout << "GLFW failed to initialize" << std::endl;
 		return -1;
 	}
 
 	window = glfwCreateWindow(640, 480, "My Window", NULL, NULL);
 	glfwMakeContextCurrent(window);
 
-	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cout << "OpenGL failed to load" << std::endl;
-		glfwTerminate();
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "GLEW failed to initialize" << std::endl;
 		return -1;
 	}
 
-	glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	float position[] = {
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	};
+
+	GLuint vertecies;
+	glGenBuffers(1, &vertecies);
+	glBindBuffer(GL_ARRAY_BUFFER, vertecies);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), position, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	float col[] = {
+		1.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 1.0f,
+		1.0f, 0.0f, 1.0f
+	};
+
+	GLuint colors;
+	glGenBuffers(1, &colors);
+	glBindBuffer(GL_ARRAY_BUFFER, colors);
+	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), col, GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(1);
+
+	std::string vertexShader = readFile("../src/shaders/shader.vert");
+	std::string fragmentShader = readFile("../src/shaders/shader.frag");
+	GLuint program = CreateShaderProgram(vertexShader, fragmentShader);
+	glUseProgram(program);
 
 	while (!glfwWindowShouldClose(window)) 
 	{
-		glfwPollEvents();
-
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		glfwSwapBuffers(window);
+
+		glfwPollEvents();
 	}
 	
 	glfwTerminate();
